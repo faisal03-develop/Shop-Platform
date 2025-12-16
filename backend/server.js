8a4b50701b671845983c5eff564d90124a9c9067
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-16 12:13:59 +0500

    Addition of the Products is Functional

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const Product = require('./models/product.model');
 
+
 dotenv.config();
 connectDB();
 
 
 
 const app = express();
+app.use(express.json());
 
 // Middleware
 app.use(express.json());
@@ -23,11 +25,16 @@ app.get('/products', async (req,res) => {
     const products = await Product.find({});
     console.log(products);
     // res.json(products);
+})
 
+app.post('/createproduct', async (req, res) => {
+    const product = req.body;
+    const resposd = await Product.create(product);
+    res.status(201).json(resposd);
 })
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
