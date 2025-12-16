26bff4ceb53235c47152a4cf69a9e52790100b4c
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-16 11:31:19 +0500

    Initized the Schema for the Products

---
+++ backend/models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        required: true
+    },
+    countInStock:{
+        type: Number,
+        required: true
+    }
+},
+{
+    timestamps: true
+}
+)
